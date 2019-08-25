b7b918510eff249bfdab3d7952279d89ecde1808
spjy <spjy@hawaii.edu>
2019-08-24 17:46:02 -1000

    feat: initial commit

---
+++ .zshrc
@@ -0,0 +1,26 @@
+# Created by newuser for 5.5.1
+source ~/antigen.zsh
+
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
+
+# Bundles from the default repo (robbyrussell's oh-my-zsh).
+antigen bundle git
+antigen bundle heroku
+antigen bundle pip
+antigen bundle lein
+antigen bundle command-not-found
+antigen bundle spotify
+
+# Syntax highlighting bundle.
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Load the theme.
+antigen theme robbyrussell
+
+# Tell Antigen that you're done.
+antigen apply
+
+alias ts3='~/Teamspeak3/ts3client_runscript.sh &'
+alias run='go run main.go'
+
---
+++ installation.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+sudo apt-get update
+
+sudo snap install discord docker insomnia
+sudo snap install code --classic
+sudo snap install tmux --classic
+sudo snap install kubectl --classic
+sudo snap install go --classic
+
+sudo apt-get install gnome-tweaks node yarn npm vim-gui-common vim-runtime
+
+curl -L git.io/antigen > ~/antigen.zsh
+
+# fonts wget https://github.com/GianCastle/FiraMonaco/raw/master/Ligamonacop.ttf
+# fonts wget https://github.com/AppleDesignResources/SanFranciscoFont/raw/master/SanFranciscoText-Regular.otf
+# cursors https://www.gnome-look.org/p/1148692/
+# theme https://www.gnome-look.org/p/1275087/
+# icons https://www.gnome-look.org/p/1309810/
+
